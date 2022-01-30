648f54b24911d1e57a9a008c0aebd0c77479adbd
FredLikesNoodles <82772444+FredLikesNoodles@users.noreply.github.com>
2022-01-30 15:48:39 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,20 @@ recog.onresult = function run(event) {
   console.log(event);
   var result = event.results[0][0].transcript;
   document.getElementById("output").innerHTML = result;
+  speak();
 };
+
+Webcam.set({
+  width: 300,
+  height: 300,
+  image_format: "png",
+  png_quality: 90,
+});
+var webcam_output = document.getElementById("webcam_output");
+Webcam.attach("#webcam_output");
+function speak() {
+  var s = webkit.speechSynthesis;
+  var output = document.getElementById("output").value;
+  var u = new SpeechSynthesisUtterance(output);
+  s.speak(u);
+}
